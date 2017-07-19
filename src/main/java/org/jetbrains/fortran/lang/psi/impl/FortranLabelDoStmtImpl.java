d89af514ca27bebbea2a343c72077573bdb4820e
satamas <semyon.atamas@jetbrains.com>
2017-07-19 22:02:42 +0300

    Remove infineite loop function

--- src/main/java/org/jetbrains/fortran/lang/psi/impl/FortranLabelDoStmtImpl.java
+++ src/main/java/org/jetbrains/fortran/lang/psi/impl/FortranLabelDoStmtImpl.java
@@ -1,23 +1,18 @@
 package org.jetbrains.fortran.lang.psi.impl;
 
-import java.util.List;
-import org.jetbrains.annotations.*;
 import com.intellij.lang.ASTNode;
 import com.intellij.psi.PsiElement;
-import com.intellij.psi.PsiElementVisitor;
-import com.intellij.psi.util.PsiTreeUtil;
-import static org.jetbrains.fortran.lang.FortranTypes.*;
-import org.jetbrains.fortran.lang.psi.*;
+import org.jetbrains.annotations.Nullable;
+import org.jetbrains.fortran.lang.psi.FortranExpr;
+import org.jetbrains.fortran.lang.psi.FortranLabelDoStmt;
+
+import static org.jetbrains.fortran.lang.FortranTypes.COLON;
+import static org.jetbrains.fortran.lang.FortranTypes.EOL;
 
 public class FortranLabelDoStmtImpl extends FortranStmtImpl implements FortranLabelDoStmt {
 
     public FortranLabelDoStmtImpl(ASTNode node) {
         super(node);
-    }
-
-    public void accept(@NotNull PsiElementVisitor visitor) {
-        if (visitor instanceof FortranVisitor) accept((FortranVisitor)visitor);
-        else super.accept(visitor);
     }
 
     @Override
