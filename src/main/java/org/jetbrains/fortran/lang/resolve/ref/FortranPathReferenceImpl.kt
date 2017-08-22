183e2fe767e3cfbd62a596801bef3eb1ed985532
sergei <sgshulman@gmail.com>
2017-08-22 12:22:26 +0300

    Some improvements and navigation between free and fixed source forms done

--- src/main/java/org/jetbrains/fortran/lang/resolve/ref/FortranPathReferenceImpl.kt
+++ src/main/java/org/jetbrains/fortran/lang/resolve/ref/FortranPathReferenceImpl.kt
@@ -10,6 +10,7 @@ import com.intellij.psi.search.FileTypeIndex
 import com.intellij.psi.search.GlobalSearchScope
 import com.intellij.util.indexing.FileBasedIndex
 import org.jetbrains.fortran.FortranFileType
+import org.jetbrains.fortran.FortranFixedFormFileType
 
 
 class FortranPathReferenceImpl(element: FortranDataPathImplMixin) :
@@ -24,22 +25,16 @@ class FortranPathReferenceImpl(element: FortranDataPathImplMixin) :
         val useStmt = PsiTreeUtil.getParentOfType(element, FortranUseStmt::class.java)
         if (useStmt != null && element.parent is FortranRenameStmt) {
             val moduleNamesPsi = useStmt.dataPath!!.reference.multiResolve().filterNotNull()
-            for (moduleNamePsi in moduleNamesPsi) {
-                val modulePsi = PsiTreeUtil.getParentOfType(moduleNamePsi, FortranModule::class.java)
 
-                if (modulePsi != null) {
-                    return modulePsi.variables.filter { element.referenceName.equals(it.name, true)}
-                            .plus(modulePsi.subprograms.filter { element.referenceName.equals(it.name, true) }).toList()
-                }
-            }
-            return emptyList()
+            return moduleNamesPsi.map { PsiTreeUtil.getParentOfType(it, FortranModule::class.java) }
+                    .filterNotNull()
+                    .flatMap { it.variables.filter { element.referenceName.equals(it.name, true)}
+                            .plus(it.subprograms.filter { element.referenceName.equals(it.name, true) }) }
+                    .toList()
         }
-
 
         val programUnit = PsiTreeUtil.getParentOfType(element, FortranProgramUnit::class.java) ?: return emptyList()
         val outerProgramUnit : FortranProgramUnit
-
-
 
         // local variables
         val names = programUnit.variables.filter { element.referenceName.equals(it.name, true) }
@@ -61,22 +56,30 @@ class FortranPathReferenceImpl(element: FortranDataPathImplMixin) :
         // other files
         val vFiles = FileBasedIndex.getInstance()
                 .getContainingFiles(FileTypeIndex.NAME, FortranFileType, GlobalSearchScope.projectScope(element.project))
+                .plus(FileBasedIndex.getInstance()
+                .getContainingFiles(FileTypeIndex.NAME, FortranFixedFormFileType, GlobalSearchScope.projectScope(element.project)))
 
         for (file in vFiles) {
-            val psiFile = PsiManager.getInstance(element.project).findFile(file) as FortranFile
+            var psiFile = PsiManager.getInstance(element.project).findFile(file)
+            if (psiFile !is FortranFile) psiFile = psiFile as FortranFixedFormFile
             names.addAll(psiFile.children.filter { it is FortranProgramUnit }
                     .map { it -> (it as FortranProgramUnit).unit }
                     .filterNotNull()
                     .filter { element.referenceName.equals(it.name, true) })
             names.addAll(psiFile.children.filter { it is FortranFunctionSubprogram }
-                    .flatMap { f -> (f as FortranFunctionSubprogram).variables.filter { element.referenceName.equals(it.name, true) } }
-                    .filterNotNull())
+                    .flatMap { f -> (f as FortranFunctionSubprogram).variables
+                            .filter { element.referenceName.equals(it.name, true)
+                                && f.unit?.name.equals(it.name, true)
+                            }
+                    }.filterNotNull())
 
         }
 
         // modules
         if (element.parent !is FortranUseStmt) { // We do not need recursion here
-            for (module in outerProgramUnit.usedModules) {
+            val allModules = outerProgramUnit.usedModules.toMutableSet()
+            if (programUnit != outerProgramUnit) allModules.addAll(programUnit.usedModules)
+            for (module in allModules) {
                 val onlyIsUsed = !(module.parent as FortranUseStmt).onlyStmtList.isEmpty()
                 val onlyList = (module.parent as FortranUseStmt).onlyStmtList
                         .map{ it.entityDecl?.name?.toLowerCase() }.filterNotNull()
