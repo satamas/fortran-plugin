d507ac6e9c672aaced7bb6d476e64814e687ca07
sergei <sgshulman@gmail.com>
2017-09-14 22:59:13 +0300

    Program unit name mismatch inspection added

---
+++ src/main/kotlin/org/jetbrains/fortran/ide/inspections/FortranConstructNameMismatchInspection.kt
@@ -0,0 +1,40 @@
+package org.jetbrains.fortran.ide.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import org.jetbrains.fortran.ide.inspections.fixes.SubstituteTextFix
+import org.jetbrains.fortran.lang.psi.*
+
+class FortranConstructNameMismatchInspection : LocalInspectionTool() {
+    override fun getDisplayName() = "Construct name mismatch"
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
+            object : FortranVisitor() {
+                override fun visitConstructName(name: FortranConstructName) {
+                    if (name.parent !is FortranExitStmt) {
+                        val construct = name.parent.parent
+                        val realName = when(construct) {
+                            is FortranAssociateConstruct -> construct.associateStmt.constructNameDecl?.name
+                            is FortranBlockConstruct -> construct.blockStmt.constructNameDecl?.name
+                            is FortranCaseConstruct -> construct.selectCaseStmt.constructNameDecl?.name
+                            is FortranCriticalConstruct -> construct.criticalStmt.constructNameDecl?.name
+                            is FortranNonlabelDoConstruct -> construct.nonlabelDoStmt.constructNameDecl?.name
+                            is FortranForallConstruct -> construct.forallConstructStmt.constructNameDecl?.name
+                            is FortranIfConstruct -> construct.ifThenStmt.constructNameDecl?.name
+                            is FortranSelectTypeConstruct -> construct.selectTypeStmt.constructNameDecl?.name
+                            is FortranWhereConstruct -> construct.whereConstructStmt.constructNameDecl?.name
+                            else -> null
+                        }
+
+                        if (!name.referenceName.equals(realName, true)) {
+                            holder.registerProblemForReference(name.reference,
+                                    ProblemHighlightType.LIKE_UNKNOWN_SYMBOL,
+                                    "Construct name mismatch",
+                                    SubstituteTextFix(name.textRange, realName, "Construct name fix")
+                            )
+                        }
+                    }
+                }
+            }
+}
---
+++ src/main/kotlin/org/jetbrains/fortran/ide/inspections/FortranProgramUnitNameMismatchInspection.kt
@@ -0,0 +1,43 @@
+package org.jetbrains.fortran.ide.inspections
+
+import com.intellij.codeInspection.LocalInspectionTool
+import com.intellij.codeInspection.ProblemHighlightType
+import com.intellij.codeInspection.ProblemsHolder
+import org.jetbrains.fortran.ide.inspections.fixes.SubstituteTextFix
+import org.jetbrains.fortran.lang.psi.*
+
+class FortranProgramUnitNameMismatchInspection : LocalInspectionTool() {
+    override fun getDisplayName() = "Program Unit name mismatch"
+
+    override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean) =
+            object : FortranVisitor() {
+                override fun visitProgramUnit(unit: FortranProgramUnit) {
+                    val stmtName = when(unit) {
+                        is FortranFunctionSubprogram -> unit.functionStmt.entityDecl?.name
+                        is FortranSubroutineSubprogram -> unit.subroutineStmt.entityDecl?.name
+                        is FortranModule -> unit.moduleStmt.entityDecl?.name
+                        is FortranSubmodule -> unit.submoduleStmt.entityDecl?.name
+                        is FortranBlockData -> unit.blockDataStmt.entityDecl?.name
+                        is FortranMainProgram -> unit.programStmt?.entityDecl?.name
+                        else -> null
+                    }
+                    val endStmtDataPath = when(unit) {
+                        is FortranFunctionSubprogram -> unit.endFunctionStmt?.dataPath
+                        is FortranSubroutineSubprogram -> unit.endSubroutineStmt?.dataPath
+                        is FortranModule -> unit.endModuleStmt?.dataPath
+                        is FortranSubmodule -> unit.endSubmoduleStmt?.dataPath
+                        is FortranBlockData -> unit.endBlockDataStmt?.dataPath
+                        is FortranMainProgram -> unit.endProgramStmt?.dataPath
+                        else -> null
+                    }
+
+                    if (endStmtDataPath != null && !endStmtDataPath.referenceName.equals(stmtName, false)) {
+                        holder.registerProblemForReference(endStmtDataPath.reference,
+                                ProblemHighlightType.LIKE_UNKNOWN_SYMBOL,
+                                "Program Unit name mismatch",
+                                SubstituteTextFix(endStmtDataPath.textRange, stmtName, "Program Unit name fix")
+                        )
+                    }
+                }
+            }
+}
