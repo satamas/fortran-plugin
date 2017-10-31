6772fa6000d385d911ba01e6168d284b7a5a52f2
Sergei Shulman <sgshulman@gmail.com>
2017-10-31 15:08:28 +0300

    Add Notification about fixed form source

---
+++ src/main/kotlin/org/jetbrains/fortran/ide/formatter/FortranSourceFormNotificationProvider.kt
@@ -0,0 +1,53 @@
+package org.jetbrains.fortran.ide.formatter
+
+import com.intellij.openapi.command.CommandProcessor
+import com.intellij.openapi.fileEditor.FileEditor
+import com.intellij.openapi.project.Project
+import com.intellij.openapi.util.Key
+import com.intellij.openapi.vfs.VirtualFile
+import com.intellij.psi.PsiManager
+import com.intellij.ui.EditorNotificationPanel
+import com.intellij.ui.EditorNotifications
+import org.jetbrains.fortran.ide.intentions.FortranFixedFormConverter
+import org.jetbrains.fortran.lang.psi.FortranFixedFormFile
+
+
+class FortranSourceFormNotificationProvider(private val myProject: Project) : EditorNotifications.Provider<EditorNotificationPanel>() {
+    private val KEY = Key.create<EditorNotificationPanel>("FixedFormSource")
+
+    override fun getKey(): Key<EditorNotificationPanel> {
+        return KEY
+    }
+
+    override fun createNotificationPanel(file: VirtualFile, fileEditor: FileEditor): EditorNotificationPanel? {
+        if (userDoesntLikeUs) return null
+        val psiFile = (PsiManager.getInstance(myProject).findFile(file) ?: return null) as? FortranFixedFormFile ?: return null
+
+        return FortranSourceFormNotificationPanel(myProject, psiFile)
+    }
+
+    private class FortranSourceFormNotificationPanel(project: Project,
+                                                     file: FortranFixedFormFile) : EditorNotificationPanel() {
+        init {
+            setText("Fixed source form is deprecated. Translate file to free form?")
+
+            createActionLabel("Translate") { translate(project, file) }.toolTipText = "Translate file"
+            createActionLabel("Hide") { hideNotification() }.toolTipText = "Hide this notification"
+        }
+
+        private fun hideNotification() {
+            isVisible = false
+            userDoesntLikeUs = true
+        }
+
+        private fun translate(project: Project, file: FortranFixedFormFile) {
+            CommandProcessor.getInstance().executeCommand(project,
+                    { FortranFixedFormConverter().invoke(project, null, file) },
+                    "Translate source to free form", null)
+        }
+    }
+
+    companion object {
+        private var userDoesntLikeUs = false
+    }
+}
