c7bf927ae5c5b4a3b6e99eebb9fb6af4b12df27e
Sergei Shulman <sgshulman@gmail.com>
2017-11-16 00:26:11 +0300

    Add tests for FortranIfStatementToConstructIntention
    Upgrade FortranIfStatementToConstructIntention

--- src/test/kotlin/org/jetbrains/fortran/ide/inspections/FortranInspectionsBaseTestCase.kt
+++ src/test/kotlin/org/jetbrains/fortran/ide/inspections/FortranInspectionsBaseTestCase.kt
@@ -1,9 +1,9 @@
 package org.jetbrains.fortran.ide.inspections
 
 import com.intellij.codeInspection.LocalInspectionTool
-import com.intellij.openapi.util.io.StreamUtil
 import com.intellij.testFramework.fixtures.LightPlatformCodeInsightFixtureTestCase
 import org.intellij.lang.annotations.Language
+import org.jetbrains.fortran.lang.FortranTestUtils
 
 abstract class FortranInspectionsBaseTestCase(
         val inspection: LocalInspectionTool
@@ -32,40 +32,17 @@ abstract class FortranInspectionsBaseTestCase(
 
     fun testInspectionHasDocumentation() {
         val description = "inspectionDescriptions/${inspection.javaClass.simpleName?.dropLast("Inspection".length)}.html"
-        val text = getResourceAsString(description)
+        val text = FortranTestUtils.getResourceAsString(description)
                 ?: error("No inspection description for ${inspection.javaClass} ($description)")
-        checkHtmlStyle(text)
+        FortranTestUtils.checkHtmlStyle(text)
     }
 
-    protected fun enableInspection() =
+    private fun enableInspection() =
             myFixture.enableInspections(inspection.javaClass)
 
-    protected fun applyQuickFix(name: String) {
+    private fun applyQuickFix(name: String) {
         val action = myFixture.findSingleIntention(name)
         myFixture.launchAction(action)
-    }
-
-    companion object {
-        @JvmStatic
-        fun checkHtmlStyle(html: String) {
-            // http://stackoverflow.com/a/1732454
-            val re = "<body>(.*)</body>".toRegex(RegexOption.DOT_MATCHES_ALL)
-            val body = (re.find(html)?.let { it.groups[1]!!.value } ?: html).trim()
-            check(body[0].isUpperCase()) {
-                "Please start description with the capital latter"
-            }
-
-            check(body.last() == '.') {
-               "Please end description with a period"
-            }
-        }
-
-        @JvmStatic fun getResourceAsString(path: String): String? {
-            val stream = FortranInspectionsBaseTestCase::class.java.classLoader.getResourceAsStream(path)
-                    ?: return null
-
-            return StreamUtil.readText(stream, Charsets.UTF_8)
-        }
     }
 }
 
