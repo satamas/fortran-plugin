fc35dd63a27a45e7c12415de375058394d46f10b
Sergei Shulman <sgshulman@gmail.com>
2017-12-08 23:41:55 +0300

    Rewrite unused label inspection to have an option to partially use it in other inspections

--- src/main/kotlin/org/jetbrains/fortran/ide/inspections/FortranUnusedLabelInspection.kt
+++ src/main/kotlin/org/jetbrains/fortran/ide/inspections/FortranUnusedLabelInspection.kt
@@ -21,25 +21,42 @@ import org.jetbrains.fortran.lang.resolve.FortranLabelReferenceImpl
 class FortranUnusedLabelInspection : LocalInspectionTool() {
     override fun buildVisitor(holder: ProblemsHolder, isOnTheFly: Boolean): FortranVisitor {
         return object : FortranVisitor() {
-
             override fun visitLabelDecl(label: FortranLabelDecl) {
-                // custom searcher for leading zeros
-                val unit = runReadAction{ PsiTreeUtil.getParentOfType(label, FortranProgramUnit::class.java)}
-                val results = runReadAction{ PsiTreeUtil.findChildrenOfType(unit, FortranLabelImpl::class.java)
-                }.filter { (label as FortranLabelDeclImpl).getLabelValue() == it.getLabelValue() }
-                        .map{ FortranLabelReferenceImpl(it as FortranLabelImplMixin) }
-
-                if (results.isEmpty()) {
-                    val lastElement = if (label.nextSibling.node.elementType != TokenType.WHITE_SPACE) label else label.nextSibling
-                    if (runReadAction{ PsiTreeUtil.getParentOfType(label, PsiFile::class.java)} is FortranFile) {
-                        holder.registerProblem(label, "Unused label declaration", ProblemHighlightType.LIKE_UNUSED_SYMBOL,
-                                SubstituteTextFix(label.smartPointer(), lastElement.smartPointer(), "", "Delete label declaration"))
-                    } else {
-                        holder.registerProblem(label, "Unused label declaration", ProblemHighlightType.LIKE_UNUSED_SYMBOL,
-                                SubstituteTextFix(label.smartPointer(), " ".repeat(label.textLength), "Delete label declaration"))
-                    }
+                if (needFix(label)) {
+                    holder.registerProblem(
+                            label,
+                            "Unused label declaration",
+                            ProblemHighlightType.LIKE_UNUSED_SYMBOL,
+                            createFix(label)
+                    )
                 }
             }
         }
     }
+
+    companion object {
+        fun needFix(label: FortranLabelDecl, maximumRefsCount: Int = 0) : Boolean {
+            // custom searcher for leading zeros
+            val unit = runReadAction{ PsiTreeUtil.getParentOfType(label, FortranProgramUnit::class.java)}
+            val results = runReadAction{ PsiTreeUtil.findChildrenOfType(unit, FortranLabelImpl::class.java)
+            }.filter { (label as FortranLabelDeclImpl).getLabelValue() == it.getLabelValue() }
+                    .map{ FortranLabelReferenceImpl(it as FortranLabelImplMixin) }
+            return results.size <= maximumRefsCount
+        }
+
+        fun createFix(label: FortranLabelDecl) : SubstituteTextFix {
+            val freeForm = runReadAction{ PsiTreeUtil.getParentOfType(label, PsiFile::class.java)} is FortranFile
+            val lastElement = if (freeForm && label.nextSibling.node.elementType != TokenType.WHITE_SPACE)
+                label
+            else
+                label.nextSibling
+            return SubstituteTextFix(
+                    label.smartPointer(),
+                    lastElement.smartPointer(),
+                    if (freeForm) "" else " ".repeat(label.textLength),
+                    "Delete label declaration"
+            )
+        }
+    }
+
 }
