125369eb2fcf8fc09d739298725ff456032c70bd
Sergei Shulman <sgshulman@gmail.com>
2022-06-24 09:44:39 +0200

    Fix operator / and // parsing

--- src/test/resources/psi/Operator.f95
+++ src/test/resources/psi/Operator.f95
@@ -1,4 +1,18 @@
 interface operator(+)
 procedure :: foo_plus, fooo_plus
 end interface
+interface operator( /)
+procedure :: foo_div
+end interface
+interface operator(/ )
+procedure :: foo_div2
+end interface
+interface operator(/ &
+! comment
+)
+procedure :: foo_div3
+end interface
+interface operator(//)
+procedure :: foo_divdiv
+end interface
 end
