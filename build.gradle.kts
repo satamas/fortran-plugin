880e10871998d1e1c54c1da0cf513a8a6fe8ec0e
Cristian Le <cristian.le@mpsd.mpg.de>
2023-08-04 14:48:54 +0200

    Split the main project into `:plugin` and `:idea` projects
    Signed-off-by: Cristian Le <cristian.le@mpsd.mpg.de>

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,10 +1,22 @@
-import org.gradle.api.tasks.testing.logging.TestExceptionFormat
-import org.jetbrains.grammarkit.tasks.GenerateLexerTask
-import org.jetbrains.grammarkit.tasks.GenerateParserTask
-import org.jetbrains.intellij.tasks.JarSearchableOptionsTask
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import groovy.xml.XmlParser
 
+// Environment settings
 val CI = System.getenv("CI") != null
+
+// Platform and IDE settings
+val platformVersion = prop("platformVersion").toInt()
+val baseIDE = prop("baseIDE")
+val ideaVersion = prop("ideaVersion")
+val clionVersion = prop("clionVersion")
+val baseVersion = when (baseIDE) {
+    "idea" -> ideaVersion
+    "clion" -> clionVersion
+    else -> error("Unexpected IDE name: `$baseIDE`")
+}
+
+val pluginVersion = prop("pluginVersion")
+val basePluginArchiveName = "fortran-plugin"
+
 
 plugins {
     id("java") // Java support
@@ -38,73 +50,163 @@ allprojects {
     }
 
     intellij {
+        version.set(baseVersion)
         downloadSources.set(!CI)
+        updateSinceUntilBuild.set(true)
         instrumentCode.set(false)
         ideaDependencyCachePath.set(file("deps").absolutePath)
+        sandboxDir.set("$buildDir/$baseIDE-sandbox-$platformVersion")
     }
 
     kotlin {
         jvmToolchain(17)
+        sourceSets {
+            main {
+                kotlin.srcDirs("src/$platformVersion/main/kotlin")
+            }
+            test {
+                kotlin.srcDirs("src/$platformVersion/test/kotlin")
+            }
+        }
     }
 
     sourceSets {
-        getByName("main").java.srcDirs("src/gen")
-    }
-    // Add jvm-default=all to allow for kotlin interface with defaults
-    // https://kotlinlang.org/docs/java-to-kotlin-interop.html#compatibility-modes-for-default-methods
-    tasks.named("compileKotlin", org.jetbrains.kotlin.gradle.tasks.KotlinCompilationTask::class.java) {
-        compilerOptions {
-            freeCompilerArgs.add("-Xjvm-default=all")
+        main {
+            java.srcDirs("src/gen")
+            resources.srcDirs("src/$platformVersion/main/resources")
+        }
+        test {
+            resources.srcDirs("src/$platformVersion/test/resources")
         }
     }
+    tasks {
+        compileKotlin {
+            compilerOptions {
+                // Add jvm-default=all to allow for kotlin interface with defaults
+                // https://kotlinlang.org/docs/java-to-kotlin-interop.html#compatibility-modes-for-default-methods
+                freeCompilerArgs.add("-Xjvm-default=all")
+            }
+        }
+        patchPluginXml {
+            sinceBuild.set(prop("sinceBuild"))
+            untilBuild.set(prop("untilBuild"))
+        }
+
+        // All these tasks don't make sense for non-root subprojects
+        // Root project (i.e. `:plugin`) enables them itself if needed
+        runIde { enabled = false }
+        prepareSandbox { enabled = false }
+        buildSearchableOptions { enabled = false }
+    }
 }
 
-project(":clion") {
+// Main project interface and metadata with run, build and publish tasks
+project(":plugin") {
+    version = "$pluginVersion.${prop("buildNumber")}"
     intellij {
-        version.set("CL-${prop("clionVersion")}")
-        plugins.set(listOf("com.intellij.cidr.base", "com.intellij.clion"))
+        pluginName.set("fortran-plugin")
     }
+
     dependencies {
         implementation(project(":"))
-    }
-    tasks.withType<JarSearchableOptionsTask> {
-        duplicatesStrategy = DuplicatesStrategy.INCLUDE
+        implementation(project(":idea"))
+        implementation(project(":clion"))
     }
 
-    tasks.named<Zip>("buildPlugin") {
-        archiveBaseName.set("fortran-plugin")
+    // Collects all jars produced by compilation of project modules and merges them into singe one.
+    // We need to put all plugin manifest files into single jar to make new plugin model work
+    val mergePluginJarTask = task<Jar>("mergePluginJars") {
+        duplicatesStrategy = DuplicatesStrategy.FAIL
+        archiveBaseName.set(basePluginArchiveName)
+
+        exclude("META-INF/MANIFEST.MF")
+        exclude("**/classpath.index")
+
+        val pluginLibDir by lazy {
+            val sandboxTask = tasks.prepareSandbox.get()
+            sandboxTask.destinationDir.resolve("${sandboxTask.pluginName.get()}/lib")
+        }
+
+        val pluginJars by lazy {
+            pluginLibDir.listFiles().orEmpty().filter { it.isPluginJar() }
+        }
+
+        destinationDirectory.set(project.layout.dir(provider { pluginLibDir }))
+
+        doFirst {
+            for (file in pluginJars) {
+                from(zipTree(file))
+            }
+        }
+
+        doLast {
+            delete(pluginJars)
+        }
     }
+
+    // Add plugin sources to the plugin ZIP.
+    // gradle-intellij-plugin will use it as a plugin sources if the plugin is used as a dependency
+    val createSourceJar = task<Jar>("createSourceJar") {
+        dependsOn(":generateLexer")
+        dependsOn(":generateParser")
+
+        for (prj in rootProject.allprojects) {
+            from(prj.kotlin.sourceSets.main.get().kotlin) {
+                include("**/*.java")
+                include("**/*.kt")
+            }
+        }
+
+        destinationDirectory.set(layout.buildDirectory.dir("libs"))
+        archiveBaseName.set(basePluginArchiveName)
+        archiveClassifier.set("src")
+    }
+
+    tasks {
+        buildPlugin {
+            dependsOn(createSourceJar)
+            from(createSourceJar) { into("lib/src") }
+            // Set proper name for final plugin zip.
+            // Otherwise, base name is the same as gradle module name
+            archiveBaseName.set(basePluginArchiveName)
+        }
+        runIde {
+            jvmArgs("-Xmx768m", "-XX:+UseG1GC", "-XX:SoftRefLRUPolicyMSPerMB=50")
+            jvmArgs("-Didea.auto.reload.plugins=false")
+            jvmArgs("-Dide.show.tips.on.startup.default.value=false")
+            enabled = true
+        }
+        prepareSandbox {
+            finalizedBy(mergePluginJarTask)
+            enabled = true
+        }
+        buildSearchableOptions {
+            dependsOn(mergePluginJarTask)
+            enabled = true
+        }
+    }
+
+    // TODO: Add changelog, description, sign and publish info grabbers
 }
 
+// Main fortran language project
 project(":") {
-    intellij {
-        version.set(prop("ideaVersion"))
-    }
-
-    val generateFortranLexer = task<GenerateLexerTask>("generateFortranLexer") {
-        sourceFile.set(file("src/main/kotlin/org/jetbrains/fortran/lang/lexer/FortranLexer.flex"))
-        targetDir.set("src/gen/org/jetbrains/fortran/lang/lexer")
-        targetClass.set("_FortranLexer")
-        purgeOldFiles.set(true)
-    }
-
-    val generateFortranParser = task<GenerateParserTask>("generateFortranParser") {
-        dependsOn(generateFortranLexer)
-        sourceFile.set(project.layout.projectDirectory.file("src/main/kotlin/org/jetbrains/fortran/lang/parser/FortranParser.bnf"))
-        targetRoot.set("src/gen")
-        targetRootOutputDir.set(project.layout.projectDirectory.dir(targetRoot.get()))
-        pathToParser.set("/org/jetbrains/fortran/lang/parser/FortranParser.java")
-        pathToPsiRoot.set("/org/jetbrains/fortran/lang/psi")
-        purgeOldFiles.set(true)
-    }
-
-    tasks.withType<KotlinCompile> {
-        dependsOn(generateFortranParser)
-    }
-
-    tasks.withType<Test> {
-        testLogging {
-            exceptionFormat = TestExceptionFormat.FULL
+    tasks {
+        generateLexer {
+            sourceFile.set(file("src/main/grammars/FortranLexer.flex"))
+            targetDir.set("src/gen/org/jetbrains/fortran/lang/lexer")
+            targetClass.set("_FortranLexer")
+            purgeOldFiles.set(true)
+        }
+        generateParser {
+            sourceFile.set(file("src/main/grammars/FortranParser.bnf"))
+            targetRoot.set("src/gen")
+            pathToParser.set("org/jetbrains/fortran/lang/parser/FortranParser.java")
+            pathToPsiRoot.set("org/jetbrains/fortran/lang/psi")
+            purgeOldFiles.set(true)
+        }
+        compileKotlin {
+            dependsOn(generateLexer, generateParser)
         }
     }
 
@@ -112,12 +214,53 @@ project(":") {
         doLast {
             rootProject.allprojects
                 .map { it.configurations }
-                .flatMap { listOf(it.getByName("compile"), it.getByName("testCompile")) }
-                    .forEach { it.resolve() }
+                .flatMap { it.filter { c -> c.isCanBeResolved } }
+                .forEach { it.resolve() }
         }
     }
 }
 
+project(":clion") {
+    intellij {
+        version.set(clionVersion)
+        plugins.set(listOf("com.intellij.cidr.base", "com.intellij.clion"))
+    }
+    dependencies {
+        implementation(project(":"))
+//        testImplementation(project(":", "testOutput"))
+    }
+}
 
+project(":idea") {
+    intellij {
+        version.set(ideaVersion)
+        plugins.set(listOf("com.intellij.java", "com.intellij.java.ide"))
+    }
+    dependencies {
+        implementation(project(":"))
+//        testImplementation(project(":", "testOutput"))
+    }
+}
+
+fun hasProp(name: String): Boolean = extra.has(name)
 fun prop(name: String): String = extra.properties[name] as? String
     ?: error("Property `$name` is not defined in gradle.properties")
+fun properties(key: String) = providers.gradleProperty(key)
+
+fun File.isPluginJar(): Boolean {
+    if (!isFile) return false
+    if (extension != "jar") return false
+    return zipTree(this).files.any { it.isManifestFile() }
+}
+
+fun File.isManifestFile(): Boolean {
+    if (extension != "xml") return false
+    val rootNode = try {
+        val parser = XmlParser()
+        parser.parse(this)
+    } catch (e: Exception) {
+        logger.error("Failed to parse $path", e)
+        return false
+    }
+    return rootNode.name() == "idea-plugin"
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,24 @@
 rootProject.name = "fortran-plugin"
 
-include(":clion")
+// Special module with run, build and publish tasks
+include("plugin")
+include(
+    "idea",
+    "clion",
+)
+
+// Configure Gradle Build Cache. It is enabled in `gradle.properties` via `org.gradle.caching`.
+buildCache {
+    local {
+        isEnabled = System.getenv("CI") == null
+        directory = File(rootDir, "build/build-cache")
+        removeUnusedEntriesAfterDays = 30
+    }
+}
+
+pluginManagement {
+    repositories {
+        maven("https://oss.sonatype.org/content/repositories/snapshots/")
+        gradlePluginPortal()
+    }
+}
