ff45aaddd40c3f0e8a0630aa18be3fafe3af404d
Semyon.Atamas <semyon.atamas@jetbrains.com>
2023-12-07 10:47:28 +0100

    Fix structure view navigation

--- src/main/kotlin/org/jetbrains/fortran/ide/structure/FortranStructureViewElement.kt
+++ src/main/kotlin/org/jetbrains/fortran/ide/structure/FortranStructureViewElement.kt
@@ -12,46 +12,82 @@ import org.jetbrains.fortran.lang.psi.*
 import org.jetbrains.fortran.lang.psi.impl.FortranProgramUnitImpl
 
 class FortranStructureViewElement(
-        val psi: FortranCompositeElement
-) : StructureViewTreeElement, Navigatable by (psi as NavigatablePsiElement) {
+    val psi: NavigatablePsiElement
+) : StructureViewTreeElement, Navigatable {
 
     override fun getValue() = psi
 
     override fun getPresentation(): ItemPresentation = when (psi) {
-    // files
+        // files
         is FortranFile -> PresentationData(psi.name, null, FortranIcons.fileTypeIcon, null)
         is FortranFixedFormFile -> PresentationData(psi.name, null, FortranIcons.fileTypeIcon, null)
 
-    // program units
-        is FortranMainProgram -> PresentationData(psi.programStmt?.text ?: "Program", null, FortranIcons.mainProgramIcon, null)
+        // program units
+        is FortranMainProgram -> PresentationData(
+            psi.programStmt?.text ?: "Program",
+            null,
+            FortranIcons.mainProgramIcon,
+            null
+        )
+
         is FortranFunctionSubprogram -> PresentationData(psi.functionStmt.text, null, FortranIcons.functionIcon, null)
-        is FortranSubroutineSubprogram -> PresentationData(psi.subroutineStmt.text, null, FortranIcons.subroutineIcon, null)
+        is FortranSubroutineSubprogram -> PresentationData(
+            psi.subroutineStmt.text,
+            null,
+            FortranIcons.subroutineIcon,
+            null
+        )
+
         is FortranModule -> PresentationData(psi.moduleStmt.text, null, FortranIcons.moduleIcon, null)
         is FortranSubmodule -> PresentationData(psi.submoduleStmt.text, null, FortranIcons.submoduleIcon, null)
-        is FortranSeparateModuleSubprogram -> PresentationData(psi.mpSubprogramStmt.text, null, FortranIcons.separateModuleSubprogramIcon, null)
+        is FortranSeparateModuleSubprogram -> PresentationData(
+            psi.mpSubprogramStmt.text,
+            null,
+            FortranIcons.separateModuleSubprogramIcon,
+            null
+        )
+
         is FortranBlockData -> PresentationData(psi.blockDataStmt.text, null, FortranIcons.blockDataIcon, null)
 
-    // types
-        is FortranDerivedTypeDef -> PresentationData(psi.derivedTypeStmt.typeDecl.text, null, FortranIcons.typeIcon, null)
+        // types
+        is FortranDerivedTypeDef -> PresentationData(
+            psi.derivedTypeStmt.typeDecl.text,
+            null,
+            FortranIcons.typeIcon,
+            null
+        )
+
         is FortranInterfaceBlock -> PresentationData(psi.interfaceStmt.text, null, FortranIcons.interfaceIcon, null)
         else -> PresentationData(psi.text, null, null, null)
     }
 
-    override fun getChildren(): Array<TreeElement> =
-            childElements.sortedBy { it.textOffset }
-                    .map(::FortranStructureViewElement).toTypedArray()
+    override fun getChildren() = childElements
+        .sortedBy { it.textOffset }
+            .map(::FortranStructureViewElement)
+            .toTypedArray()
 
-    private val childElements: List<FortranCompositeElement>
+    override fun navigate(requestFocus: Boolean) = psi.navigate(requestFocus)
+
+    override fun canNavigate() = psi.canNavigate()
+
+    override fun canNavigateToSource() = psi.canNavigateToSource()
+
+    private val childElements: List<NavigatablePsiElement>
         get() {
             return when (psi) {
                 is FortranFile -> psi.programUnits
                 is FortranFixedFormFile -> psi.programUnits
                 is FortranProgramUnit -> {
-                    val subprograms = psi.subprograms.filter { it.parent is FortranBeginUnitStmt }.map { it.parent.parent as FortranCompositeElement }
-                    val types = psi.types.map { it.parent.parent as FortranCompositeElement }
-                    val interfaces = PsiTreeUtil.getChildrenOfType((psi as FortranProgramUnitImpl).getBlock(), FortranInterfaceBlock::class.java)?.toList() ?: emptyList()
+                    val subprograms = psi.subprograms.filter { it.parent is FortranBeginUnitStmt }
+                        .map { it.parent.parent as NavigatablePsiElement }
+                    val types = psi.types.map { it.parent.parent as NavigatablePsiElement }
+                    val interfaces = PsiTreeUtil.getChildrenOfType(
+                        (psi as FortranProgramUnitImpl).getBlock(),
+                        FortranInterfaceBlock::class.java
+                    )?.toList() ?: emptyList()
                     subprograms + types + interfaces
                 }
+
                 is FortranInterfaceBlock -> psi.interfaceSpecification?.programUnitList?.filterNotNull() ?: emptyList()
 
                 else -> emptyList()
